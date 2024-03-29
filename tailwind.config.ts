08ad0cf7b663de107eeef02eebb099c9ec2a7c1f
Wygor Fonseca <wygorfonseca@hotmail.com>
2024-03-29 17:05:23 -0300

    feat(tailwind.config.ts): added tailwindTypography plugin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
+import tailwindTypography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   darkMode: "class",
+  plugins: [tailwindTypography],
 };
