b8acbbdfafc9f761313db862d6a4f6d90ccd1c08
Wygor Fonseca <wygorfonseca@hotmail.com>
2024-03-30 20:01:25 -0300

    fix: remove signup notfound link

--- pages/index.vue
+++ pages/index.vue
@@ -23,7 +23,6 @@
           <div class="mt-10 flex flex-wrap gap-x-6 gap-y-3 justify-center">
             <a
               class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-full text-base gap-x-2.5 px-3.5 py-2.5 shadow-sm text-white dark:text-gray-900 bg-gray-900 hover:bg-gray-800 disabled:bg-gray-900 dark:bg-white dark:hover:bg-gray-100 dark:disabled:bg-white focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
-              href="/signup"
             >
               <span class="">Entre em contato</span>
               <ChatBubbleOvalLeftIcon class="w-5 h-5" />
