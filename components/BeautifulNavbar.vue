d8bb1c525e7bbdb842bd1194e4412f67de7e911d
Wygor Fonseca <wygorfonseca@hotmail.com>
2024-03-30 20:04:20 -0300

    fix: color mode first toggle

--- components/BeautifulNavbar.vue
+++ components/BeautifulNavbar.vue
@@ -17,7 +17,7 @@
           class="focus:outline-none focus-visible:outline-0 disabled:cursor-not-allowed disabled:opacity-75 flex-shrink-0 font-medium rounded-md text-sm gap-x-1.5 p-1.5 text-gray-700 dark:text-gray-200 hover:text-gray-900 dark:hover:text-white hover:bg-gray-50 dark:hover:bg-gray-800 focus-visible:ring-inset focus-visible:ring-2 focus-visible:ring-primary-500 dark:focus-visible:ring-primary-400 inline-flex items-center"
         >
           <component
-            :is="colorMode.preference === 'dark' ? SunIcon : MoonIcon"
+            :is="colorMode.preference === 'light' ? MoonIcon : SunIcon"
             class="w-5 h-5 ms-auto text-gray-700 dark:text-gray-200"
           />
         </button>
@@ -31,6 +31,6 @@ import { SunIcon, MoonIcon } from "@heroicons/vue/20/solid";
 const colorMode = useColorMode();
 
 const toggleColorMode = () => {
-  colorMode.preference = colorMode.preference === "dark" ? "light" : "dark";
+  colorMode.preference = colorMode.preference === "light" ? "dark" : "light";
 };
 </script>
